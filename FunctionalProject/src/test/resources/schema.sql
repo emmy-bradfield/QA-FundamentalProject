0fc6eddebb57b85605669d0f01ff14ebe6358ef9
emmy-bradfield <emily-bradfield@outlook.com>
2022-07-21 23:26:02 +0100

    cleaned old code out

--- FunctionalProject/src/test/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `storedb`.`Customers` VALUES (1, 'Emily', 'Bradfield');
-INSERT INTO `storedb`.`Items` VALUES (1, 'sample', 1.20);
-INSERT INTO `storedb`.`Orders` VALUES (1, 1, '2022-07-21', 1, 1, 1);
